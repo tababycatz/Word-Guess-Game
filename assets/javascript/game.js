53685dfcd85071d8f39e075143caad66a9cf8732
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-07-09 17:53:25 -0400

    started js via pseudocoding

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+//array of words..done//
+//random pick word//
+//how many letters
+// replace letters w/ underscores
+// set var for # guesses correct/wrong
+// onkey func tracking user input
+
+var candyBrands = [
+    "Twizzlers"
+    "Skittles"
+    "Sour Patch Kids"
+    "Reese's"
+    "Jolly Rancher"
+    "M&M"
+    "Twix"
+    "Kit Kat"
+    "Snickers"
+    "Starburst"
+    "Airheads"
+    "Jelly Belly"
+    "Warheads"
+    "Butterfinger"
+    "Nerds"
+    "Haribo"
+
+
+];
+var randomCandy =
+candyBrands[Math.floor(Math.random()*candyBrands.length)]
+var rightLetters = ""
+var wrongLetters = ""
